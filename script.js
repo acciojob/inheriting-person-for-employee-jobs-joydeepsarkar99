3601b5f7f07212afc49ac64c3420798feb6d8f13
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-04-21 18:11:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name, age) {
 		this.age = age;
 	}
 	greet(){
-		return `Hello, my name is ${this.name}, I am ${this.age} years old.`;
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 	}
 }
 
@@ -15,7 +15,7 @@ function Employee extends Person(name, age, jobTitle) {
 		this.jobTitle = jobTitle
 	}
 	jobGreet(){
-		return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`;
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 	}
 }
 
